23302be8fe79af00c65182d99deb0e9ae96637c6
khja231229 <khja231229@gmail.com>
2024-02-28 17:12:57 +0900

    20240228_JOIN

---
+++ General/Scripts/WORKBOOK/WORKBOOK-1번.sql
@@ -0,0 +1,29 @@
+-- 1번
+-- 춘 기술대학교의 학과 이름과 계열을 조회하시오.
+-- 단, 출력 헤더(컬럼명)는 "학과 명", "계열"으로 표시하도록 한다.
+
+SELECT DEPARTMENT_NAME "학과 명", CATEGORY "계열"
+FROM TB_DEPARTMENT;
+
+
+-- 2 번
+-- 학과의 학과 정원을 다음과 같은 형태로 조회하시오
+SELECT 
+	DEPARTMENT_NAME || '의 정원은' || CAPACITY || '명 입니다.' "학과별 정원"
+FROM TB_DEPARTMENT;
+
+
+
+-- 4번
+-- 도서관에서 대출 도서 장기 연체자들을 찾아 이름을 게시하고자 한다.
+-- 그 대상자들의 학번이 다음과 같을 때 대상자들을 찾는 적절한 SQL구문을 작성하시오.
+-- A513079, A513090, A513091, A513110, A513119
+SELECT STUDENT_NAME
+FROM TB_STUDENT
+WHERE STUDENT_NO IN ('A513079', 'A513090', 'A513091', 'A513110', 'A513119')
+ORDER BY STUDENT_NAME DESC;
+
+
+
+
+
